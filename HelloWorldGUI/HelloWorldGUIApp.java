e6844613575f4b257326c74d743331e6df84d941
Nayeon Shin <nayeon.codes@gmail.com>
2022-01-28 09:57:30 -0500

    feat: Update HelloWorldGUIApp.java

--- HelloWorldGUI/HelloWorldGUIApp.java
+++ HelloWorldGUI/HelloWorldGUIApp.java
@@ -13,7 +13,7 @@ public class HelloWorldGUIApp {
 				JFrame frame = new JFrame("HelloWorld GUI");
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setPreferredSize(new Dimension(800, 300));
-				JLabel label = new JLabel("Hello World!!", SwingConstants.RIGHT);
+				JLabel label = new JLabel("Hello, World!", SwingConstants.RIGHT);
 				frame.getContentPane().add(label);
 				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 				frame.setLocation(dim.width / 2 - 400 / 2, dim.height / 2 - 300 / 2);
