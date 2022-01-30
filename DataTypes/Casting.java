cacc5599e4aecc42f4e95b07516c0945d00f8315
Nayeon Shin <nayeon.codes@gmail.com>
2022-01-30 15:02:20 -0500

    feat(DataTypes): Create Variables.java and Casting.java

---
+++ DataTypes/Casting.java
@@ -0,0 +1,19 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		double b = 1; // Not erroneous; we don't lose anything
+		System.out.println(b);
+
+//		int c = 1.1; // Erroneous; we lose the fractional part
+		double d = 1.1;
+		int e = (int) 1.1;
+		System.out.println(e);
+
+		// 1 to String
+		String f = Integer.toString(1);
+		System.out.println(f.getClass());
+	}
+
+}
---
+++ DataTypes/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a = 1; // integer
+		System.out.println(a);
+
+		double b = 1.1; // double (real number)
+		System.out.println(b);
+
+		String c = "Hello, World!";
+		System.out.println(c);
+	}
+
+}
