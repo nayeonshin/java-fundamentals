c90f3dc100f942e7243ca484d405e16f68b811a4
Nayeon Shin <nayeon.codes@gmail.com>
2022-01-30 14:33:33 -0500

    feat(DataTypes): Create StringApp.java

---
+++ DataTypes/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// Character vs. String
+		System.out.println("Hello, World!"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+
+		System.out.println("Hello, " + "World!");
+
+		// New line
+		System.out.println("Hello,\nWorld!");
+
+		// Escape
+		System.out.println("Hello, \"World!\""); // Hello, "World!"
+	}
+
+}
