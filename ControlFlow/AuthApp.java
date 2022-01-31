0c16e9d54174d338446f86389a82186efd86c3a6
Nayeon Shin <nayeon.codes@gmail.com>
2022-01-31 18:32:39 -0500

    feat(ControlFlow): Create IfApp.java and AuthApp.java

---
+++ ControlFlow/AuthApp.java
@@ -0,0 +1,24 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+
+		System.out.println(args[0]);
+
+		String id = "nayeon";
+		String inputId = args[0];
+
+		String password = "1111";
+		String inputPassword = args[1];
+
+		System.out.println("Hi!");
+
+		if (inputId.equals(id) && inputPassword.equals(password)) {
+			System.out.println("Master!");
+		} else {
+			System.out.println("Who are you?");
+		}
+
+	}
+
+}
---
+++ ControlFlow/IfApp.java
@@ -0,0 +1,12 @@
+
+public class IfApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("a");
+		System.out.println(2);
+		System.out.println("b");
+
+	}
+
+}
