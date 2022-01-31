6eafe7b77f52cb873b657959f118919d1e2d18eb
Nayeon Shin <nayeon.codes@gmail.com>
2022-01-31 18:05:47 -0500

    feat(ControlFlow): Create BooleanApp.java

---
+++ ControlFlow/BooleanApp.java
@@ -0,0 +1,20 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("One");
+		System.out.println(1);
+
+		System.out.println(true);
+		System.out.println(false);
+
+		String greeting = "Hello, World!";
+//		String true = "Hello, World!"; // Reserved word
+
+		System.out.println(greeting.contains("World"));
+		System.out.println(greeting.contains("Nayeon"));
+
+	}
+
+}
