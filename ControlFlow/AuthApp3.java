cac08ded9dc0d31a064230171b4ccde25778af76
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-08 23:19:09 -0500

    feat(ControlFlow): Use 2d array to store passwords in `AuthApp3.java`

--- ControlFlow/AuthApp3.java
+++ ControlFlow/AuthApp3.java
@@ -3,13 +3,14 @@ public class AuthApp3 {
 
 	public static void main(String[] args) {
 
-		String[] users = { "nayeon", "nayeony", "nayoni" };
+		String[][] users = { { "nayeon", "1111" }, { "nayeony", "2222" }, { "nayoni", "3333" } };
 		String inputId = args[0];
+		String inputPassword = args[1];
 
 		boolean isSignedIn = false;
 		for (int i = 0; i < users.length; i++) {
-			String currentId = users[i];
-			if (currentId.equals(inputId)) {
+			String[] currentUser = users[i];
+			if (currentUser[0].equals(inputId) && currentUser[1].equals(inputPassword)) {
 				isSignedIn = true;
 				break;
 			}
@@ -17,7 +18,7 @@ public class AuthApp3 {
 
 		System.out.println("Hi,");
 		if (isSignedIn) {
-			System.out.println("Master");
+			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
 		}
