8ad9cf314f88d312eec213c971e96e217883bad7
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-12 16:10:51 -0500

    refactor(MyApp): `AccountingApp.java` into methods

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -1,29 +1,22 @@
 
 public class AccountingApp {
 
+	public static double valueOfSupply = 10000.0; // 공급가액
+	public static double vatRate = 0.1; // 부가가치세율
+
+	public static double getVAT() {
+		return valueOfSupply * vatRate;
+	}
+
+	public static double getTotal() {
+		return valueOfSupply + getVAT();
+	}
+
 	public static void main(String[] args) {
 
-		double valueOfSupply = Double.parseDouble(args[0]);
-		double vatRate = 0.1;
-		double expenseRate = 0.3;
-		double vat = valueOfSupply * vatRate; // Value-added tax
-		double total = valueOfSupply + vat;
-		double expense = valueOfSupply * expenseRate;
-		double income = valueOfSupply - expense;
-
-		// Dividend: 5 : 3 : 2
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
-
 		System.out.println("Value of supply: " + valueOfSupply);
-		System.out.println("VAT: " + vat);
-		System.out.println("Total: " + total);
-		System.out.println("Expense: " + expense); // 30%
-		System.out.println("Income: " + income);
-		System.out.println("Dividend 1: " + dividend1);
-		System.out.println("Dividend 2: " + dividend2);
-		System.out.println("Dividend 3: " + dividend3);
+		System.out.println("VAT: " + getVAT());
+		System.out.println("Total: " + getTotal());
 
 	}
 
