b0cfc5e097d7e9a69625d21f30fbbf5e49bb3540
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-12 17:15:11 -0500

    feat(Methods): Create `AccessLevelModifiers.java`

---
+++ Methods/AccessLevelModifiers.java
@@ -0,0 +1,19 @@
+
+class Greeting {
+
+	// public, protected, default, private
+	public static void printHi() {
+		System.out.println("Hi");
+	}
+
+}
+
+public class AccessLevelModifiers {
+
+	public static void main(String[] args) {
+
+		Greeting.printHi();
+
+	}
+
+}
