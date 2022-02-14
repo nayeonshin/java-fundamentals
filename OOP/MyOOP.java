9047ae1a5f242a3edd668c843bb368d47604f6f7
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-14 11:13:23 -0500

    feat(OOP): Instantiate `Printer`

--- OOP/MyOOP.java
+++ OOP/MyOOP.java
@@ -1,17 +1,25 @@
 public class MyOOP {
 
 	public static void main(String[] args) {
-		Printer.delimiter = "----";
-		Printer.printA();
-		Printer.printA();
-		Printer.printB();
-		Printer.printB();
+		Printer printer1 = new Printer();
+		printer1.delimiter = "----";
+		printer1.printA();
+		printer1.printA();
+		printer1.printB();
+		printer1.printB();
 
-		Printer.delimiter = "****";
-		Printer.printA();
-		Printer.printA();
-		Printer.printB();
-		Printer.printB();
+		Printer printer2 = new Printer();
+		printer2.delimiter = "****";
+		printer2.printA();
+		printer2.printA();
+		printer2.printB();
+		printer2.printB();
+
+		printer1.printA();
+		printer2.printA();
+		printer1.printA();
+		printer2.printA();
+
 	}
 
 }
--- OOP/Printer.java
+++ OOP/Printer.java
@@ -1,15 +1,15 @@
 
 class Printer {
 
-	public static String delimiter;
+	public String delimiter;
 
-	public static void printA() {
+	public void printA() {
 		System.out.println(delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 
-	public static void printB() {
+	public void printB() {
 		System.out.println(delimiter);
 		System.out.println("B");
 		System.out.println("B");
