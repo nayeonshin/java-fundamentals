5be019c6e3565a3202e823d92efcfc2e1bdd2870
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-14 07:11:06 -0500

    refactor(OOP): `Printer` class out of `MyOOP.java`

--- OOP/MyOOP.java
+++ OOP/MyOOP.java
@@ -1,21 +1,3 @@
-class Printer {
-
-	public static String delimiter;
-
-	public static void printA() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	}
-
-	public static void printB() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	}
-
-}
-
 public class MyOOP {
 
 	public static void main(String[] args) {
---
+++ OOP/Printer.java
@@ -0,0 +1,18 @@
+
+class Printer {
+
+	public static String delimiter;
+
+	public static void printA() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+	public static void printB() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	}
+
+}
