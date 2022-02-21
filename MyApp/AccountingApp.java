bf44ae6235514d8f6239129da9a6d8891c47179e
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-20 20:06:40 -0500

    refactor(MyApp): Instantiate `AccountingCalculator` and create constructor

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -1,13 +1,17 @@
 class AccountingCalculator {
 
-	public static double valueOfSupply; // 공급가액
+	public double valueOfSupply; // 공급가액
 	public static double vatRate = 0.1; // 부가가치세율
 
-	public static double getVAT() {
+	public AccountingCalculator(double valueOfSupply) {
+		this.valueOfSupply = valueOfSupply;
+	}
+
+	public double getVAT() {
 		return valueOfSupply * vatRate;
 	}
 
-	public static double getTotal() {
+	public double getTotal() {
 		return valueOfSupply + getVAT();
 	}
 
@@ -17,11 +21,14 @@ public class AccountingApp {
 
 	public static void main(String[] args) {
 
-		AccountingCalculator.valueOfSupply = 10000.0;
+		AccountingCalculator accounting1 = new AccountingCalculator(10000.0);
+		AccountingCalculator accounting2 = new AccountingCalculator(20000.0);
 
-		System.out.println("Value of supply: " + AccountingCalculator.valueOfSupply);
-		System.out.println("VAT: " + AccountingCalculator.getVAT());
-		System.out.println("Total: " + AccountingCalculator.getTotal());
+		System.out.println("VAT: " + accounting1.getVAT());
+		System.out.println("VAT: " + accounting2.getVAT());
+
+		System.out.println("Total: " + accounting1.getTotal());
+		System.out.println("Total: " + accounting2.getTotal());
 
 	}
 
