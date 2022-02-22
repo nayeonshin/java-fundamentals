83273d7acee78ec4eac1754eb30e1e54d38ca209
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-21 23:50:26 -0500

    feat(Inheritance): Create `InheritanceApp.java`

---
+++ Inheritance/InheritanceApp.java
@@ -0,0 +1,38 @@
+class Calculator {
+
+	public int add(int value1, int value2) {
+		return value1 + value2;
+	}
+
+}
+
+class ChildCalculator extends Calculator {
+	
+	
+}
+
+public class InheritanceApp {
+
+	public static void main(String[] args) {
+
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.add(2, 1));
+
+		ChildCalculator calculator2 = new ChildCalculator();
+		System.out.println(calculator2.add(2,  1));
+		
+	}
+
+}
+
+class Calculator2 {
+
+	public int add(int value1, int value2) {
+		return value1 + value2;
+	}
+
+	public int subtract(int value1, int value2) {
+		return value1 - value2;
+	}
+
+}
