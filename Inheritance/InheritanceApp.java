7290a4639ca340ae1de40688a963af2c0a4ccdca
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-23 08:21:07 -0500

    feat(Inheritance): Add constructors

--- Inheritance/InheritanceApp.java
+++ Inheritance/InheritanceApp.java
@@ -1,26 +1,29 @@
 class Calculator {
 
-	public int add(int value1, int value2) {
-		return value1 + value2;
+	int value1;
+	int value2;
+
+	Calculator(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+		System.out.println("Calculator init");
 	}
 
-	// Overloading
-	public int add(int value1, int value2, int value3) {
-		return this.add(value1, value2) + value3;
+	public int add() {
+		return this.value1 + this.value2;
 	}
 
 }
 
 class ChildCalculator extends Calculator {
 
-	@Override
-	public int add(int value1, int value2) {
-		System.out.println("ChildCalculator");
-		return super.add(value1, value2);
+	ChildCalculator(int value1, int value2) {
+		super(value1, value2);
+		System.out.println("ChildCalculator init");
 	}
 
-	public int subtract(int value1, int value2) {
-		return value1 - value2;
+	public int subtract() {
+		return this.value1 - this.value2;
 	}
 
 }
@@ -29,27 +32,11 @@ public class InheritanceApp {
 
 	public static void main(String[] args) {
 
-		Calculator calculator = new Calculator();
-		System.out.println(calculator.add(2, 1));
-		System.out.println(calculator.add(2, 1, 1));
+		Calculator calculator = new Calculator(2, 1);
+		ChildCalculator calculator2 = new ChildCalculator(2, 1);
+		System.out.println(calculator2.add()); // 3
+		System.out.println(calculator2.subtract()); // 1
 
-		ChildCalculator calculator2 = new ChildCalculator();
-		System.out.println(calculator2.add(2, 1));
-		System.out.println(calculator2.subtract(2, 1));
-		System.out.println(calculator2.add(2, 1));
-
-	}
-
-}
-
-class Calculator2 {
-
-	public int add(int value1, int value2) {
-		return value1 + value2;
-	}
-
-	public int subtract(int value1, int value2) {
-		return value1 - value2;
 	}
 
 }
