04ebe30c65c386607c0e89323a2a0428b74a9576
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-26 13:17:18 -0500

    feat(Exception): Add `try`/`catch` in `ExceptionApp.java`

--- Exception/ExceptionApp.java
+++ Exception/ExceptionApp.java
@@ -3,8 +3,23 @@ public class ExceptionApp {
 	public static void main(String[] args) {
 
 		System.out.println(1);
-		System.out.println(2 / 0);
-		System.out.println(3);
+
+		int[] scores = { 10, 20, 30 };
+
+		try {
+			System.out.println(2);
+			System.out.println(scores[3]);
+			// 실행될 기회를 아예 갖지 못하는 코드
+			System.out.println(3);
+			System.out.println(2 / 0);
+			System.out.println(4);
+		} catch (ArithmeticException e) {
+			System.out.println("Wrong calculation.");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("You're looking for a nonexistent value.");
+		}
+
+		System.out.println(5);
 
 	}
 
