b87f2bbde68354f8defdc79ce9dfd9089037a90f
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-26 13:53:38 -0500

    feat(Exception): Use `catch (Exception e)` in `ExceptionApp.java`

--- Exception/ExceptionApp.java
+++ Exception/ExceptionApp.java
@@ -11,12 +11,12 @@ public class ExceptionApp {
 			System.out.println(scores[3]);
 			// 실행될 기회를 아예 갖지 못하는 코드
 			System.out.println(3);
-			System.out.println(2 / 0);
+//			System.out.println(2 / 0);
 			System.out.println(4);
 		} catch (ArithmeticException e) {
 			System.out.println("Wrong calculation.");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("You're looking for a nonexistent value.");
+		} catch (Exception e) {
+			System.out.println("Something is wrong. An error happened.");
 		}
 
 		System.out.println(5);
