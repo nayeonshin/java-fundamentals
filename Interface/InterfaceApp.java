6107d20d3c3aceeab54abc7cfe1730dee36725fb
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-25 20:06:40 -0500

    feat(Interface): Add `Printable` and rename `Calculator` to `Calculable`

--- Interface/InterfaceApp.java
+++ Interface/InterfaceApp.java
@@ -1,17 +1,32 @@
-interface Calculator {
+interface Calculable {
+
+	double pi = 3.14;
+
 	int add(int value1, int value2);
+
 }
 
-class RealCalculator implements Calculator {
+interface Printable {
+
+	void print();
+
+}
+
+class RealCalculator implements Calculable, Printable {
 
 	@Override
 	public int add(int value1, int value2) {
 		return value1 + value2;
 	}
 
+	@Override
+	public void print() {
+		System.out.println("This is RealCalculator.");
+	}
+
 }
 
-class DummyCalculator implements Calculator {
+class DummyCalculator implements Calculable {
 
 	@Override
 	public int add(int value1, int value2) {
@@ -26,6 +41,8 @@ public class InterfaceApp {
 
 		RealCalculator calculator = new RealCalculator();
 		System.out.println(calculator.add(2, 1));
+		calculator.print();
+		System.out.println(calculator.pi);
 
 	}
 
