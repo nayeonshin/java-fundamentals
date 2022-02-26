67295dd8fb915bd2e0c97ac6f7d56550f5e591f4
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-26 14:14:25 -0500

    feat(Exception): Add `e.getMessage()` and `e.printStackTrace()`

--- Exception/ExceptionApp.java
+++ Exception/ExceptionApp.java
@@ -8,13 +8,13 @@ public class ExceptionApp {
 
 		try {
 			System.out.println(2);
-			System.out.println(scores[3]);
-			// 실행될 기회를 아예 갖지 못하는 코드
+//			System.out.println(scores[3]);
 			System.out.println(3);
-//			System.out.println(2 / 0);
+			System.out.println(2 / 0);
 			System.out.println(4);
 		} catch (ArithmeticException e) {
-			System.out.println("Wrong calculation.");
+			System.out.println("Wrong calculation. " + e.getMessage());
+			e.printStackTrace();
 		} catch (Exception e) {
 			System.out.println("Something is wrong. An error happened.");
 		}
