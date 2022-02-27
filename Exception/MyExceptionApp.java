a5fb7908bd3992cda90dca5c69a5569f2541b20c
Nayeon Shin <nayeon.codes@gmail.com>
2022-02-26 20:12:53 -0500

    feat(Exception): Create `MyExceptionApp` & `ThrowExceptionApp`

---
+++ Exception/MyExceptionApp.java
@@ -0,0 +1,9 @@
+public class MyExceptionApp {
+
+	public static void main(String[] args) {
+
+		throw new RuntimeException("Something is wrong.");
+
+	}
+
+}
---
+++ Exception/ThrowExceptionApp.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThrowExceptionApp {
+
+	public static void main(String[] args) throws IOException {
+
+		FileWriter fileWriter = new FileWriter("./data.txt");
+
+	}
+
+}
